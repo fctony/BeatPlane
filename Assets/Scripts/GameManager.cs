a6fb954ee428de665486c348eebe1fdb3566575e
fctony <13556406120@163.com>
2018-06-21 23:28:11 +0800

    plane

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameState{
+	Runing,
+	Pause
+}
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager _instance;
+
+	public int score=0;
+
+	private GUIText guiText;
+	public GameState gameState = GameState.Runing;
+
+	void Awake(){
+		_instance=this;
+		guiText=GameObject.FindGameObjectWithTag("ScoreGUI").GetComponent<GUIText>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text="Score:"+score;
+	}
+	public void transfromGameState(){
+		if(gameState==GameState.Runing){
+			pauseGame();
+		}else if(gameState==GameState.Pause){
+			continueGame();
+		}
+	}
+
+	public void pauseGame(){
+		Time.timeScale=0;// time.delatTime = 0
+		gameState=GameState.Pause;
+	}
+	public void continueGame(){
+		Time.timeScale=1;
+		gameState=GameState.Runing;
+	}
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+    public static GameOver _instance;
+
+    public GUIText highScore;
+    public GUIText nowScore;
+
+    void Awake() {
+        _instance = this;
+        this.gameObject.SetActive(false);
+    }
+
+    public void Show(float nowScore) {
+        float historyScore = PlayerPrefs.GetFloat("historyHighScore", 0);
+        if (nowScore > historyScore) {
+            PlayerPrefs.SetFloat("historyHighScore", nowScore);
+        }
+        highScore.text = historyScore + "";
+        this.nowScore.text = nowScore + "";
+
+        this.gameObject.SetActive(true);
+    }
+
+
+}
